7e2c9096a7c1779b680e673a7431e67efcca0c4f
Taewoo Kim <taewkim@cisco.com>
2020-05-26 03:36:05 -0400

    checking Stream

--- lib/screens/wraper.dart
+++ lib/screens/wraper.dart
@@ -1,9 +1,13 @@
+import 'package:brew_ottawa/models/user.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'authenticate/authenticate.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
+    print('user - $user');
     // return either Home or Authetnicate Widget
     return Authenticate();
   }
