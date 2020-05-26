f60976e1162d0f308bec714639aa7b659991ef63
Taewoo Kim <taewkim@cisco.com>
2020-05-26 02:55:44 -0400

    import firebase auth - anonymous

--- lib/screens/wraper.dart
+++ lib/screens/wraper.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'home/home.dart';
+import 'authenticate/authenticate.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // return either Home or Authetnicate Widget
-    return Home();
+    return Authenticate();
   }
 }
