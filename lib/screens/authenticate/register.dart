2227387fea7735235d5ced56763746f90766b0f2
Taewoo Kim <taewkim@cisco.com>
2020-05-26 14:22:22 -0400

    adding sign in and sign up page

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,16 +1,15 @@
 import 'package:brew_ottawa/services/auth.dart';
 import 'package:flutter/material.dart';
 
-class SignIn extends StatefulWidget {
+class Register extends StatefulWidget {
   @override
-  _SignInState createState() => _SignInState();
+  _RegisterState createState() => _RegisterState();
 }
 
-class _SignInState extends State<SignIn> {
-
+class _RegisterState extends State<Register> {
+  
   final AuthService _auth = AuthService();
-
-  // text field state
+    // text field state
   String email = '';
   String password = '';
 
@@ -21,7 +20,7 @@ class _SignInState extends State<SignIn> {
       appBar: AppBar(
         backgroundColor: Colors.brown[400],
         elevation: 0.0,
-        title: Text('Sign in to Brew Crew'),
+        title: Text('Sign up to Brew Crew'),
       ),
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 15.0),
@@ -45,7 +44,7 @@ class _SignInState extends State<SignIn> {
               RaisedButton(
                 color: Colors.pink[400],
                 child: Text(
-                  'Sign In',
+                  'Register',
                   style: TextStyle(color: Colors.white)
                 ),
                 onPressed: () async {
