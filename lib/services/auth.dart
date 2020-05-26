e9c97d4e586cc19d5c7e4ae4fd87ee45ecb43ae9
Taewoo Kim <taewkim@cisco.com>
2020-05-26 03:17:54 -0400

    Firebase Stream for onAuthStateChanged

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -11,6 +11,13 @@ class AuthService {
     return user != null ? User(uid: user.uid) : null;
   }
 
+  // auth change user stream
+  Stream<User> get user {
+    return _auth.onAuthStateChanged
+      .map((FirebaseUser user) => _userFromFirebaseUser(user));
+  }
+
+
   // sign in anonymously
   Future signInAnon () async {
     try {
