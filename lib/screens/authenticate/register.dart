687493801a7414527bb82f86aad4fd40944536c3
Taewoo Kim <taewkim@cisco.com>
2020-05-27 17:21:36 -0400

    register with email and password

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -13,9 +13,12 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
   
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
+
     // text field state
   String email = '';
   String password = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
@@ -38,10 +41,12 @@ class _RegisterState extends State<Register> {
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 15.0),
         child: Form(
+          key: _formKey,
           child: Column(
             children: <Widget>[
               SizedBox(height: 20.0,),
               TextFormField(
+                validator: (val) => val.isEmpty ? 'Enter an email' : null,
                 onChanged: (val) {
                   setState(() => email = val);
                 },
@@ -49,6 +54,7 @@ class _RegisterState extends State<Register> {
               SizedBox(height: 20.0,),
               TextFormField(
                 obscureText: true,
+                validator: (val) => val.length < 6 ? 'Enter a password > 6 ' : null,
                 onChanged: (val) {
                   setState(() => password = val);
                 },
@@ -61,9 +67,18 @@ class _RegisterState extends State<Register> {
                   style: TextStyle(color: Colors.white)
                 ),
                 onPressed: () async {
-                  print(email);
-                  print(password);
+                  if (_formKey.currentState.validate()) {
+                    dynamic result = await _auth.registerWithEmailAndPassword(email, password);
+                    if (result == null) {
+                      setState(() => error = 'please supplay a valid email');
+                    }
+                  }
                 },
+              ),
+              SizedBox(height: 20.0,),
+              Text(
+                error,
+                style: TextStyle(color: Colors.red, fontSize: 14.0)
               )
             ]
           ),
